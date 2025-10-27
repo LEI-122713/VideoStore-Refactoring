3afb62d9cc6a5936556a92968e0f6d52c77f78b1
nxzua <nxzua@iscte-iul.pt>
2025-10-27 22:05:07 +0000

    Move Method, Rename

--- VideoStore_Refactoring/src/v6/Movie.java
+++ VideoStore_Refactoring/src/v6/Movie.java
@@ -2,6 +2,34 @@ package v6;
 
 public class Movie {
 
+	public double getRentalAmount(int rental)
+	{
+		double result = 0;
+
+		switch (_priceCode)
+		{
+			case REGULAR:
+				result += 2;
+				if (rental > 2)
+					result += (rental - 2) * 1.5;
+				break;
+			case NEW_RELEASE:
+				result += rental * 3;
+				break;
+			case CHILDRENS:
+				result += 1.5;
+				if (rental > 3)
+					result += (rental - 3) * 1.5;
+				break;
+		}
+		return result;
+	}
+
+	public int getFrequentRentalPoints(int rental)
+	{
+		return (_priceCode == Code.NEW_RELEASE) && rental > 1 ? 2 : 1;
+	}
+
 	public enum Code {REGULAR, CHILDRENS, NEW_RELEASE};
 	
 	private String _title;
