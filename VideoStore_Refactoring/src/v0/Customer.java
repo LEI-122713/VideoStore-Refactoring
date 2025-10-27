9fb7004156c9a3459cce6eb67b0b1b5569c37356
nxzua <nxzua@iscte-iul.pt>
2025-10-27 21:20:22 +0000

    Extract Method

--- VideoStore_Refactoring/src/v0/Customer.java
+++ VideoStore_Refactoring/src/v0/Customer.java
@@ -32,25 +32,7 @@ public class Customer
 		
 		for (Rental each: _rentals)
 		{
-			double thisAmount = 0;
-
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode())
-			{
-				case REGULAR:
-					thisAmount += 2;
-					if (each.getDaysRented() > 2)
-						thisAmount += (each.getDaysRented() - 2) * 1.5;
-					break;
-				case NEW_RELEASE:
-					thisAmount += each.getDaysRented() * 3;
-					break;
-				case CHILDRENS:
-					thisAmount += 1.5;
-					if (each.getDaysRented() > 3)
-						thisAmount += (each.getDaysRented() - 3) * 1.5;
-					break;
-			}
+			double thisAmount = getAmount(each);
 
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -68,5 +50,28 @@ public class Customer
 		result += "Amount owed is " + totalAmount + "\n";
 		result += "You earned " + frequentRenterPoints + " frequent renter points";
 		return result;
+	}
+
+	private static double getAmount(Rental each) {
+		double thisAmount = 0;
+
+		// determine amounts for each line
+		switch (each.getMovie().getPriceCode())
+		{
+			case REGULAR:
+				thisAmount += 2;
+				if (each.getDaysRented() > 2)
+					thisAmount += (each.getDaysRented() - 2) * 1.5;
+				break;
+			case NEW_RELEASE:
+				thisAmount += each.getDaysRented() * 3;
+				break;
+			case CHILDRENS:
+				thisAmount += 1.5;
+				if (each.getDaysRented() > 3)
+					thisAmount += (each.getDaysRented() - 3) * 1.5;
+				break;
+		}
+		return thisAmount;
 	}
 }
