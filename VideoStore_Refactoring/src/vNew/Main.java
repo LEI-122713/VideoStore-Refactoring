3eba794b5bac1ecb20a697cf5acc5664d08bbce5
LEI-122713 <jaafe@iscte-iul.pt>
2025-10-30 10:19:49 +0000

    refactor(v1): Move Method getAmount() de Customer para Rental (Feature Envy)

--- VideoStore_Refactoring/src/vNew/Main.java
+++ VideoStore_Refactoring/src/vNew/Main.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.Scanner;
 
-import demo.Database;
-
 /***********************************************************
  * Filename: Main.java
  * 
