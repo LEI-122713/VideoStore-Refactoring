c34aea9936d86c5d4deb512c5dadf574819cacd1
LEI-122713 <jaafe@iscte-iul.pt>
2025-10-30 10:37:32 +0000

    Replace Temp with Query

--- VideoStore_Refactoring/src/v4/Customer.java
+++ VideoStore_Refactoring/src/v4/Customer.java
@@ -2,46 +2,51 @@ package v4;
 
 import java.util.Vector;
 
-public class Customer
-{
-	private String			_name;
-	private Vector<Rental>	_rentals	= new Vector<Rental>();
+public class Customer {
+    private String _name;
+    private Vector<Rental> _rentals = new Vector<Rental>();
 
-	public Customer(String _name)
-	{
-		this._name = _name;
-	}
+    public Customer(String _name) {
+        this._name = _name;
+    }
 
-	public void addRental(Rental arg)
-	{
-		_rentals.addElement(arg);
-	}
+    public void addRental(Rental arg) {
+        _rentals.addElement(arg);
+    }
 
-	public String getName()
-	{
-		return _name;
-	}
+    public String getName() {
+        return _name;
+    }
 
-	public String statement()
-	{
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
+    public String statement() {
+        // header
+        String result = "Rental Record for " + getName() + "\n";
 
-		// header
-		String result = "Rental Record for " + getName() + "\n";
-		
-		for (Rental each: _rentals)
-		{
-			frequentRenterPoints += each.getFrequentRentalPoints();
+        for (Rental each : _rentals) {
+            // show figures for this rental
+            result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
+        }
 
-			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + each.getAmount() + "\n";
-			totalAmount += each.getAmount();
-		}
+        // add footer lines (usando as novas queries)
+        result += "Amount owed is " + getTotalAmount() + "\n";
+        result += "You earned " + getTotalFrequentRenterPoints() + " frequent renter points";
 
-		// add footer lines
-		result += "Amount owed is " + totalAmount + "\n";
-		result += "You earned " + frequentRenterPoints + " frequent renter points";
-		return result;
-	}
+        return result;
+    }
+
+    private double getTotalAmount() {
+        double result = 0;
+        for (Rental each : _rentals) {
+            result += each.getAmount();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        for (Rental each : _rentals) {
+            result += each.getFrequentRentalPoints();
+        }
+        return result;
+    }
 }
